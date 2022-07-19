76ae56a4ba3e10cd7a0f6467e80e6291611176e8
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-19 15:11:56 -0500

    game functional, wrote expressions to check player and enemy health

--- assets/js/game.js
+++ assets/js/game.js
@@ -16,7 +16,22 @@ var fight = function() {
 enemyHealth = enemyHealth - playerAttack;
 console.log(playerName + " attacked " + enemyName + ". " + enemyName + " has " + enemyHealth + " health remaining.");
 
+//check enemy's health
+if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health remaning.");
+}
+
 playerHealth = playerHealth - enemyAttack;
 console.log(enemyName + " attacked " + playerName + ". " + playerName + " has " + playerHealth + " remaning.");
+
+if(playerHealth <= 0) {
+    window.alert(playerName + " has died!")
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+}
 
 fight();
