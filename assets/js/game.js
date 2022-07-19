235f7a1e43ba1346815e781147fa2f2e6328b338
Patrick Kinsley <102868418+pwkinsley87@users.noreply.github.com>
2022-07-19 13:59:05 -0500

    wrote fight function with health and attack variable for player and enemy robots. first battle round is functional and logging in Dev tools console.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,22 @@
-window.alert("You can do this! It's not too late!");
+var playerName = window.prompt("And what shall we call the young warrior?");
+var playerHealth = 100;
+var playerAttack =10;
+
+//Multiple values can be logged like so: 
+console.log(playerName, playerAttack, playerHealth);
+
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+};
+
+enemyHealth = enemyHealth - playerAttack;
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " has " + enemyHealth + " health remaining.");
+
+playerHealth = playerHealth - enemyAttack;
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " has " + playerHealth + " remaning.");
+
+fight();
